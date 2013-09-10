68b1eab051882f944564d3e81244c7bd8cff061c
kodamirmo <irvingemmanuelgg@gmail.com>
2013-09-09 23:57:14 -0500

    Add splash and login screen

--- LostAndFound/gen/com/pawhub/lostandfound/R.java
+++ LostAndFound/gen/com/pawhub/lostandfound/R.java
@@ -21,29 +21,53 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int pawhub_site=0x7f020001;
-        public static final int splash_logo=0x7f020002;
+        public static final int bottom_corner_round_gray=0x7f020000;
+        public static final int corner_and_border_white_editext=0x7f020001;
+        public static final int corner_round_bottom=0x7f020002;
+        public static final int corner_round_list=0x7f020003;
+        public static final int corner_roundwhite=0x7f020004;
+        public static final int fbsign=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int login_btn=0x7f020007;
+        public static final int or_line=0x7f020008;
+        public static final int oval=0x7f020009;
+        public static final int pawhub_site=0x7f02000a;
+        public static final int splash_logo=0x7f02000b;
+        public static final int twsign=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int splash_app_subtittle=0x7f080001;
-        public static final int splash_app_tittle=0x7f080000;
+        public static final int action_settings=0x7f08000b;
+        public static final int editTextNombre=0x7f080003;
+        public static final int editTextPassword=0x7f080004;
+        public static final int loginBtn=0x7f080005;
+        public static final int loginFbBtn=0x7f080007;
+        public static final int loginTwBtn=0x7f080008;
+        public static final int login_app_subtittle=0x7f080002;
+        public static final int login_app_tittle=0x7f080001;
+        public static final int login_logo=0x7f080000;
+        public static final int login_or=0x7f080006;
+        public static final int splash_app_subtittle=0x7f08000a;
+        public static final int splash_app_tittle=0x7f080009;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
-        public static final int activity_splash=0x7f030001;
+        public static final int activity_login_screen=0x7f030001;
+        public static final int activity_splash=0x7f030002;
     }
     public static final class menu {
         public static final int home=0x7f070000;
-        public static final int splash=0x7f070001;
+        public static final int login_screen=0x7f070001;
+        public static final int splash=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int app_newpass=0x7f050008;
+        public static final int app_registrer=0x7f050007;
         public static final int app_subtitle=0x7f050005;
         public static final int app_title=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_login_screen=0x7f050006;
         public static final int title_activity_splash=0x7f050003;
     }
     public static final class style {
--- LostAndFound/src/com/pawhub/lostandfound/SplashScreen.java
+++ LostAndFound/src/com/pawhub/lostandfound/SplashScreen.java
@@ -1,17 +1,23 @@
 package com.pawhub.lostandfound;
 
 import com.pawhub.lostandfound.constants.Constants;
+import com.pawhub.lostandfound.preferences.ConfigData;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 
 public class SplashScreen extends Activity {
 
+	private Activity activity;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		
+		activity=this;
 		
 		AsyncTaskSplash splash=new AsyncTaskSplash();
 		splash.execute();
@@ -32,7 +38,18 @@ public class SplashScreen extends Activity {
 		}
 		
 		protected void onPostExecute(Void param){
-
+			
+			Intent loginScreen;
+			
+			//Change condition when login has been implemented
+			if(!ConfigData.isLogged(activity))
+				loginScreen=new Intent(getApplicationContext(),LoginScreen.class);
+			else
+				loginScreen=new Intent(getApplicationContext(),Home.class);
+			
+			loginScreen.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			getBaseContext().startActivity(loginScreen);
+			activity.finish();
 		}	
 		
 	}
