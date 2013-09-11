0d11b292f0e208d41163a98e875d9a4f444004eb
kodamirmo <irvingemmanuelgg@gmail.com>
2013-09-10 21:28:15 -0500

    Add Report base class

--- LostAndFound/src/com/pawhub/lostandfound/transferobjects/Report.java
+++ LostAndFound/src/com/pawhub/lostandfound/transferobjects/Report.java
@@ -1,5 +1,15 @@
 package com.pawhub.lostandfound.transferobjects;
 
-public class Report {
-
+public abstract class Report {
+	
+	public static final int TYPE_DOG=0;
+	public static final int TYPE_CAT=1;
+	public static final int TYPE_OTHER=2;
+	
+	private String idReport;
+	private int petType;
+	private String lastlocation;
+	private Boolean hasPicture;
+	private String pathPicture;
+	
 }
