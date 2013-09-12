f297dd69fb0fe2a6897dad9bbc2d3f0963a4a52d
kodamirmo <irvingemmanuelgg@gmail.com>
2013-09-12 18:54:46 -0500

    Add map

--- LostAndFound/src/com/pawhub/lostandfound/FragmentCasesMap.java
+++ LostAndFound/src/com/pawhub/lostandfound/FragmentCasesMap.java
@@ -1,5 +1,8 @@
 package com.pawhub.lostandfound;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class FragmentCasesMap extends Fragment{
+	
+	private GoogleMap map;
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
@@ -16,6 +21,12 @@ public class FragmentCasesMap extends Fragment{
 	@Override
     public void onActivityCreated(Bundle state) {
         super.onActivityCreated(state);
-       
+
+        Fragment fragment=getFragmentManager().findFragmentById(R.id.myMap);
+        
+        SupportMapFragment fragmentoDeMapa=(SupportMapFragment)fragment;
+
+    	map=fragmentoDeMapa.getMap();
+
     }
 }
