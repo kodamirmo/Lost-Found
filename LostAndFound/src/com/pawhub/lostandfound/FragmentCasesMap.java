002fe7f0b8d9050c8604eb29937c9edc11c79f77
kodamirmo <irvingemmanuelgg@gmail.com>
2013-09-12 19:29:03 -0500

    Zoom map

--- LostAndFound/src/com/pawhub/lostandfound/FragmentCasesMap.java
+++ LostAndFound/src/com/pawhub/lostandfound/FragmentCasesMap.java
@@ -1,7 +1,11 @@
 package com.pawhub.lostandfound;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -22,11 +26,24 @@ public class FragmentCasesMap extends Fragment{
     public void onActivityCreated(Bundle state) {
         super.onActivityCreated(state);
 
-        Fragment fragment=getFragmentManager().findFragmentById(R.id.myMap);
-        
-        SupportMapFragment fragmentoDeMapa=(SupportMapFragment)fragment;
-
-    	map=fragmentoDeMapa.getMap();
+        initMap();
 
     }
+	
+	private void initMap(){
+		
+		Fragment fragment=getFragmentManager().findFragmentById(R.id.myMap);
+        SupportMapFragment fragmentoDeMapa=(SupportMapFragment)fragment;
+    	map=fragmentoDeMapa.getMap();
+    	
+    	LatLng coordenada=new LatLng(19.4326018,-99.1332049);
+    	
+    	CameraPosition camPos = new CameraPosition.Builder()
+        .target(coordenada).zoom(12).build();
+    	
+    	CameraUpdate cameraUpdate =
+        	    CameraUpdateFactory.newCameraPosition(camPos);
+    	
+    	map.moveCamera(cameraUpdate);
+	}
 }
