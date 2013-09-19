01d019a4e3eb1d901133205f134d26426f9b9bde
asukita <pvc38@hotmail.com>
2013-09-18 22:03:32 -0500

    adding map in Report layout, minimal changes in detail_chart, activity_detail portrait and landscape layouts

--- LostAndFound/src/com/pawhub/lostandfound/ReportActivity.java
+++ LostAndFound/src/com/pawhub/lostandfound/ReportActivity.java
@@ -1,59 +1,44 @@
 package com.pawhub.lostandfound;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class ReportActivity extends ActionBarActivity {
-	
-	private String currentTitle;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class ReportActivity extends FragmentActivity {
+
+	private GoogleMap map;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_report);
 
-		initSlidingMenu();
-	}
+		map = ((SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.reportMap)).getMap();
+		
+		LatLng coordenada = new LatLng(19.4326018, -99.1332049);
+		CameraPosition camPos = new CameraPosition.Builder().target(coordenada)
+				.zoom(12).build();
 
-	private void initSlidingMenu() {
+		CameraUpdate cameraUpdate = CameraUpdateFactory
+				.newCameraPosition(camPos);
 
-		final ActionBar actionBar = getSupportActionBar();
+		map.moveCamera(cameraUpdate);
 
-		currentTitle = "";
-
-		/*
-		 * mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-		 * mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
-		 * GravityCompat.START);
-		 */
-
-		actionBar.setDisplayHomeAsUpEnabled(true);
-		actionBar.setHomeButtonEnabled(true);
-		actionBar.setIcon(R.drawable.app_name2);
-		actionBar.setBackgroundDrawable(new ColorDrawable(Color
-				.parseColor("#B71C4E")));
-		actionBar.setTitle(currentTitle);
-
-		/*
-		 * mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-		 * R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
-		 * public void onDrawerClosed(View view) {
-		 * actionBar.setTitle(currentTitle); supportInvalidateOptionsMenu(); }
-		 * 
-		 * public void onDrawerOpened(View drawerView) {
-		 * actionBar.setTitle(currentTitle); supportInvalidateOptionsMenu(); }
-		 * }; mDrawerLayout.setDrawerListener(mDrawerToggle);
-		 */
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.report, menu);
+		
 		return true;
 	}
 
