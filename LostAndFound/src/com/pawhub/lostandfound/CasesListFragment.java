19ceca77504dc613b7d22ec61eb7dc35da0269f4
kodamirmo <irvingemmanuelgg@gmail.com>
2013-10-09 22:11:13 -0500

    Delete some functions

--- LostAndFound/src/com/pawhub/lostandfound/CasesListFragment.java
+++ LostAndFound/src/com/pawhub/lostandfound/CasesListFragment.java
@@ -2,6 +2,9 @@ package com.pawhub.lostandfound;
 
 import com.pawhub.lostandfound.transferobjects.AbuseReport;
 import com.pawhub.lostandfound.transferobjects.AccidentReport;
+import com.pawhub.lostandfound.transferobjects.FoundReport;
+import com.pawhub.lostandfound.transferobjects.HomelessReport;
+import com.pawhub.lostandfound.transferobjects.LostReport;
 import com.pawhub.lostandfound.transferobjects.Report;
 import com.pawhub.lostandfound.views.ReportView;
 
@@ -83,11 +86,24 @@ public class CasesListFragment extends Fragment{
 		AccidentReport report4=new AccidentReport("ABCCD", Report.TYPE_CAT, "", null,
 				"Este es un comentario acerca de la situacion", false, 234, false, "Pedro Paramo");
 		
+		FoundReport report5 =new FoundReport("ADSFE", Report.TYPE_OTHER, "", null, 
+				"Este es un comentario acerca de la situacion", 2, "rojo", "", false, 0, true, "Laura Bozo");
+		
+		LostReport report6 =new LostReport("ADASDASF", Report.TYPE_CAT, "", "d", 
+				"Este es un comentario acerca de la situacion", "un año", 2,"azul", "gato",false, 456, false, "Irving Emmanuel");
+		
+		HomelessReport report7 =new HomelessReport("ASDA", Report.TYPE_DOG, "", null,
+				"Este es un comentario acerca de la situacion", true, 23, true, "Paulina Rubio");
+		
+		
 		
 		parentLayout.addView(new ReportView(report, inflater).getReportChart());
 		parentLayout.addView(new ReportView(report2, inflater).getReportChart());
 		parentLayout.addView(new ReportView(report3, inflater).getReportChart());
 		parentLayout.addView(new ReportView(report4, inflater).getReportChart());
+		parentLayout.addView(new ReportView(report5, inflater).getReportChart());
+		parentLayout.addView(new ReportView(report6, inflater).getReportChart());
+		parentLayout.addView(new ReportView(report7, inflater).getReportChart());
 	}
 	
 	private void initScreenAlerts(){
@@ -115,15 +131,4 @@ public class CasesListFragment extends Fragment{
 		
 	}
 	
-	
-	private void addDetailChart(){
-		LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService( Context.LAYOUT_INFLATER_SERVICE );
-		View v=inflater.inflate(R.layout.detail_chart_1, null);
-		parentLayout.addView(v);
-	}
-	
-	private void addDetailChart2(){
-		LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService( Context.LAYOUT_INFLATER_SERVICE );
-		inflater.inflate(R.layout.detail_chart_2,parentLayout);
-	}
 }
